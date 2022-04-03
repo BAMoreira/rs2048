bb8e6b7ea84c3ced218e0668a06532cf576ed73c
Bernardo de Azevedo Moreira <bam@discente.ifpe.edu.br>
2022-04-03 17:59:06 -0300

    Add struct type Bd along with deref and gen functions for an easily usable board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::ops::{Deref, DerefMut};
+
+struct Bd {
+    board:Vec<Vec<u32>>,
+}
+
+impl Deref for Bd {
+    type Target = Vec<Vec<u32>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.board
+    }
+}
+
+impl DerefMut for Bd {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.board
+    }
+}
+
+impl Bd {
+    fn gen(a:usize) -> Bd {
+        Bd { board: vec![vec![0;a];a], }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let board = Bd::gen(4);
 }
