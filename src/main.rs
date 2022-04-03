0bf2c81512146bbdd90ef79b2b33ddde27d77014
Bernardo de Azevedo Moreira <bam@discente.ifpe.edu.br>
2022-04-03 18:19:51 -0300

    Add detailed comments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::ops::{Deref, DerefMut};
 
+// Main board struct type
+// All should be designed around this as methods
 struct Bd {
-    board:Vec<Vec<u32>>,
-    score: u32,
+    board:Vec<Vec<u32>>, // nested vectors for a two-dimension array
+    score: u32, // score inside board for future ease when implementing move method
 }
 
+// Deref traits for Bd to call it as matrix
 impl Deref for Bd {
     type Target = Vec<Vec<u32>>;
 
@@ -12,14 +15,17 @@ impl Deref for Bd {
         &self.board
     }
 }
-
 impl DerefMut for Bd {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.board
     }
 }
 
+// Methods and associate functions for Bd
+// Pretty much most of the functions will be in here
 impl Bd {
+    // Generate a new empty board with variable size
+    // Both board and scores should be all zero, with spawn added later
     fn gen(a:usize) -> Bd {
         Bd {
             board: vec![vec![0;a];a],
@@ -28,8 +34,9 @@ impl Bd {
     }
 }
 
+// Main function, keep it small
 fn main() {
-    let mut board = Bd::gen(4);
-    board.score = 1;
+    let mut board = Bd::gen(4); // Main board on game unless future implementations of multi-boards
+
 
 }
