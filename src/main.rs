4437f5fc30ad898cb2c5d75e412a1ae880f2a4df
Bernardo de Azevedo Moreira <bam@discente.ifpe.edu.br>
2022-04-04 00:59:06 -0300

    Add GNU GPL 3.0 to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+/**************************************************************************************************
+*                                                                                                 *
+*    Copyright 2022 Bernardo de Azevedo Moreira                                                   *
+*                                                                                                 *
+*    This file is part of BAMoreira/rs2048.                                                       *
+*                                                                                                 *
+*    rs2048 is free software: you can redistribute it and/or modify it under the terms of the GNU *
+*    General Public License as published by the Free Software Foundation, either version 3 of the *
+*    License, or (at your option) any later version.                                              *
+*                                                                                                 *
+*    rs2048 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without  *
+*    even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the    *
+*    GNU General Public License for more details.                                                 *
+*                                                                                                 *
+*    You should have received a copy of the GNU General Public License along with this file.      *
+*    If not, see http://www.gnu.org/licenses/.                                                    *
+*                                                                                                 *
+**************************************************************************************************/
+
 use std::ops::{Deref, DerefMut};
 
 // Enum to identify directions
